aa0e8dc77714107e35c63a5abea36a0646c577bd
18MackeyR <ryanmackey08@gmail.com>
2019-03-29 20:58:03 -0500

    Months and Days
    Console.log months and days (not in order yet)

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-    // Moment
-var moment = require('moment');
-moment().format();
-
-    // Moment-range
+// lodash.js
+var _ = require('lodash');
+// moment-range.js
 const Moment = require('moment');
 const MomentRange = require('moment-range');
-
 const moment = MomentRange.extendMoment(Moment);
 
-    // Lodash
-// Load the full build.
-var _ = require('lodash');
-// Load the core build.
-var _ = require('lodash/core');
-// Load the FP build for immutable auto-curried iteratee-first data-last methods.
-var fp = require('lodash/fp');
- 
-// Load method categories.
-var array = require('lodash/array');
-var object = require('lodash/fp/object');
- 
-// Cherry-pick methods for smaller browserify/rollup/webpack bundles.
-var at = require('lodash/at');
-var curryN = require('lodash/fp/curryN');
+// Start of Project
+
+const range = moment.range('2019-01-01', '2019-12-31');
+
+// for (let month of range.by('month')) {
+//   month.format('YYYY-MM-DD');
+// }
+
+// Months Array
+const month = Array.from(range.by('month'));
+var arraym = month.map(m => m.format('MMMM'))
+
+console.log(arraym)
+
+// Days Array
+const days = Array.from(range.by('days'));
+var arrayd = days.map(m => m.format('DD'))
+
+console.log(arrayd)
+
+// var newarrayd = _.chunk(arrayd, 7)
+
+// console.log(newarrayd)

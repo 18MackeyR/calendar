40359144d6115d8a89cf179471f521e8e8211903
18MackeyR <ryanmackey08@gmail.com>
2019-04-01 00:55:20 -0500

    Update index.js
    The WeekDays are lined up with each day. I need to shift the first of each month with its corresponding weekday however.

--- index.js
+++ index.js
@@ -9,40 +9,40 @@ const chalk = require('chalk');
 
 // Year - Month - Day
 const yearRange = moment.range('2019-01-01', '2019-12-31');
-const weekDayRange = moment.range('2019-01-06', '2019-01-12');
+// const weekDayRange = moment.range('2019-01-06', '2019-01-12');
 
 // Month 1
-const mRange1 = moment.range('2019-01-01', '2019-01-31');
+const mRange1 = moment.range('2019-01-01','2019-01-31');
 // Month 2
-const mRange2 = moment.range('2019-02-01', '2019-02-28');
+const mRange2 = moment.range('2019-02-01','2019-02-28');
 // Month 3
-const mRange3 = moment.range('2019-03-01', '2019-03-31');
+const mRange3 = moment.range('2019-03-01','2019-03-31');
 // Month 4
-const mRange4 = moment.range('2019-04-01', '2019-04-30');
+const mRange4 = moment.range('2019-04-01','2019-04-30');
 // Month 5
-const mRange5 = moment.range('2019-05-01', '2019-05-31');
+const mRange5 = moment.range('2019-05-01','2019-05-31');
 // Month 6
-const mRange6 = moment.range('2019-06-01', '2019-06-30');
+const mRange6 = moment.range('2019-06-01','2019-06-30');
 // Month 7
-const mRange7 = moment.range('2019-07-01', '2019-07-31');
+const mRange7 = moment.range('2019-07-01','2019-07-31');
 // Month 8
-const mRange8 = moment.range('2019-08-01', '2019-08-31');
+const mRange8 = moment.range('2019-08-01','2019-08-31');
 // Month 9
-const mRange9 = moment.range('2019-09-01', '2019-09-30');
+const mRange9 = moment.range('2019-09-01','2019-09-30');
 // Month 10
-const mRange10 = moment.range('2019-10-01', '2019-10-31');
+const mRange10 = moment.range('2019-10-01','2019-10-31');
 // Month 11
-const mRange11 = moment.range('2019-11-01', '2019-11-30');
+const mRange11 = moment.range('2019-11-01','2019-11-30');
 // Month 12
-const mRange12 = moment.range('2019-12-01', '2019-12-31');
+const mRange12 = moment.range('2019-12-01','2019-12-31');
 
     function calendar(){
 
         const month = Array.from(yearRange.by('month'));
         var arraym = month.map(m => m.format('MMMM'))
     
-        const weekdays = Array.from(weekDayRange.by('days'));
-        var arrayw = weekdays.map(m => m.format('ddd'));
+        // const weekdays = Array.from(weekDayRange.by('days'));
+        // var arrayw = weekdays.map(m => m.format('ddd'));
     
         const months = ["day1","day2","day3","day4","day5","day6",
         "day7","day8","day9","day10","day11","day12",]
@@ -52,24 +52,29 @@ const mRange12 = moment.range('2019-12-01', '2019-12-31');
     
         var ranges = [mRange1,mRange2,mRange3,mRange4,mRange5,mRange6,
         mRange7,mRange8,mRange9,mRange10,mRange11,mRange12]
-    
+        
         for (i=0; i < 12;i++){
 
             months[i] = Array.from(ranges[i].by('days'));
             mapping[i] = months[i].map(m => m.format('D'));
-
             console.log(arraym[i]) // Months
 
+            console.log(_.pad('Sun  Mon  Tue  Wed  Thu  Fri  Sat', 42, '-')) // WeekDays
 
-            console.log(_.join(arrayw));
 
-            var join = _.join(mapping[i]);
+            // console.log(_.join(arrayw)); // WeekDays 
+            // if ( ranges[i] == mRange6){
+            //     console.log(chalk.red(mapping));
+            // }
+
             console.log(_.chunk(mapping[i], 7))
+            var join = _.join(ranges[i]);
+            console.log(join)
+
         }
     }
 
     calendar();
-   
 
     // TESTING 
 
